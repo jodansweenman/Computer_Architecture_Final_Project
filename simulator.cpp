ef67528a7c8cfa217c0548116ebbbe095a0f0715
bthorn2 <bthorn2@pdx.edu>
2021-05-02 11:26:14 -0700

    fixed formatting

--- simulator.cpp
+++ simulator.cpp
@@ -21,10 +21,8 @@ int main(){
 // Functional simulator (Different action based on opcode)
 // Keeps track of register and memory state changes
 
-// Read instruction type/opcode
-
 switch(opcode) {
-    // Arithmetic Instructions
+// Arithmetic Instructions
     case 000000: 
     // ADD Rd Rs Rt (Add the contents of regs Rs and Rt, transfer result to reg Rd)
         break; 
@@ -34,15 +32,18 @@ switch(opcode) {
     case 000010: 
     // SUB Rd Rs Rt (Subtract the contents of reg Rt from Rs, transfer result to reg Rd)
         break; 
-    case 000011: // SUBI Rt Rs Imm (Subtract the immediate value from the contents of reg Rs, transfer result to reg Rt)
+    case 000011: 
+    // SUBI Rt Rs Imm (Subtract the immediate value from the contents of reg Rs, transfer result to reg Rt)
         break;
     case 000100: 
     // MUL Rd Rs Rt (Multiply the contents of regs Rs and Rt, transfer result to reg Rd)
         break;
-    case 000101: // MULI Rt Rs Imm (Multiply the contents of reg Rs with the immediate value, transfer result to reg Rt)
+    case 000101: 
+    // MULI Rt Rs Imm (Multiply the contents of reg Rs with the immediate value, transfer result to reg Rt)
         break;
-    // Logical Instructions
-    case 000110: // OR Rd Rs Rt (Take a bitwise OR of the contents of regs Rs and Rt, result to Rd)
+// Logical Instructions
+    case 000110: 
+    // OR Rd Rs Rt (Take a bitwise OR of the contents of regs Rs and Rt, result to Rd)
         break;
     case 000111: 
     // ORI Rt Rs Imm (Take a bitwise OR of the contents of reg Rs and the immediate value, result to Rt)
@@ -59,7 +60,7 @@ switch(opcode) {
     case 001011:
      // XORI Rt Rs Imm (Take a bitwise XOR or the contents of Reg Rs and the immediate value, result to reg Rt)
         break;
-    // Memory Access Instructions
+// Memory Access Instructions
     case 001000: 
     // LDW Rd Rs Imm (Add the contents of Rs and the immediate value to generate the effective address "A",
     // load the contents (32-bits) of the memory location at address "A" into reg Rt)
