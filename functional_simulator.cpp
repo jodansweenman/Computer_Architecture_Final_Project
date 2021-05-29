f9bef005329a42abc74d78f8ca7bc0a0a9796248
bthorn2 <bthorn2@pdx.edu>
2021-05-29 16:54:38 -0700

    Using forwarding numbers are correct

--- functional_simulator.cpp
+++ functional_simulator.cpp
@@ -169,8 +169,8 @@ void Instruction_decoder::print_results(int registers[], struct statistics *stat
     cout << "Memory access instructions: " << stats->memory << "\n";
     cout << "Control transfer instructions: " << stats->control << "\n\n";
     cout << "Clock cycles with out forwarding: " << stats->clock_cycles_nfw << "\n";
+    cout << "Total stalls with out forwarding: " << stats->stalls_nfw << "\n\n";
     cout << "Clock cycles with forwarding: " << stats->clock_cycles_fw << "\n";
-    cout << "Total stalls with out forwarding: " << stats->stalls_nfw << "\n";
     cout << "Total stalls with forwarding: " << stats->stalls_fw << "\n";
     for(int i = 0; i < 32; i++)
     {
